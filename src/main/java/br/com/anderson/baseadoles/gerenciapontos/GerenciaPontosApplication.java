1581610b43c7b847d205e43553739a000b4711e2
Anderson Matos <ams.bns2021@gmail.com>
2023-01-15 17:02:34 -0300

    first commit

---
+++ src/main/java/br/com/anderson/baseadoles/gerenciapontos/GerenciaPontosApplication.java
@@ -0,0 +1,13 @@
+package br.com.anderson.baseadoles.gerenciapontos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaPontosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaPontosApplication.class, args);
+	}
+
+}
